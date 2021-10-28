cb50a090d82562d5961c8f14eb049a6451915f98
RTBlanco <ronnytoribio1@hotmail.com>
2021-10-28 12:36:21 -0400

    converted to typescript

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const buttonsDiv = document.getElementById('buttons')!
+  const rightDiv = document.getElementById("right")!;
+  const leftDiv = document.getElementById("left")!;
+
+
+
+  const state = {
+    split: false
+  }
+
+  rightDiv.addEventListener('click', (e) => {
+    if (state.split) {
+      rightDiv.setAttribute('style',"transform: translate(-50%);")
+      leftDiv.setAttribute('style', "transform: translate(50%);" )
+      // rightDiv.style = "transform: translate(-50%);"
+      // leftDiv.style = "transform: translate(50%);"  
+    } else {
+      rightDiv.setAttribute('style',"transform: translate(0%);")
+      leftDiv.setAttribute('style', "transform: translate(0%);" )
+      // rightDiv.style = "transform: translate(0%);"
+      // leftDiv.style = "transform: translate(0%);"
+    }
+    state.split = !state.split
+  })
+})
